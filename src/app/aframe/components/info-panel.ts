d4786173b826f781490ea6beb79c18292df1a3ee
Michele Paternoster <pianomaik@gmail.com>
2021-04-03 00:38:48 +0200

    infoPanel

--- src/app/aframe/components/info-panel.ts
+++ src/app/aframe/components/info-panel.ts
@@ -46,15 +46,17 @@ export class InfoPanel {
         entityEl.setAttribute('rotation', {x: 0, y: 90, z: 0});
         entityEl.setAttribute('material', {color: '#333333', shader: 'flat', transparent: false});
         entityEl.innerHTML = `
-        <a-plane class="" id="panelCloseBtn" position="1.157 0.810 0.003" geometry="height: 0.140; width: 0.140"></a-plane>
-        <a-plane id="img" position="0.005 0.188 0.003" crossorigin="anonymous" material="src:`+this.art.imgSrc+`; shader: flat; color: #FFF"></a-plane>
-        <a-entity id="movieTitle" position="-0.626 0.744 0" text="shader: msdf; anchor: left; width: 1.5; font: https://cdn.aframe.io/examples/ui/Viga-Regular.json; color: white; value:`+ this.art.title +`"></a-entity>`;
+        <a-plane class="clickable" id="panelCloseBtn" position="1.157 0.810 0.003" geometry="height: 0.140; width: 0.140"></a-plane>
+        <a-entity id="title" position="-0.626 0.744 0" text="shader: msdf; anchor: left; width: 1.5; font: https://cdn.aframe.io/examples/ui/Viga-Regular.json; color: white; value:`+ this.art.title +`"></a-entity>
+        <a-entity id="description" position="-0.626 0.900 0" text="shader: msdf; anchor: left; width: 1.5; font: https://cdn.aframe.io/examples/ui/Viga-Regular.json; color: white; value:`+ this.art.description + `"></a-entity>
+        `;
+
         sceneEl.appendChild(entityEl);
         entityEl.addEventListener('loaded', () => {
           this.infoPanelEl = entityEl;
           this.panelCloseBtn = document.getElementById('panelCloseBtn');
           panelCloseBtn.addEventListener('click', () => {
-            //this.closePanel();
+            this.closePanel();
           })
         });
       }

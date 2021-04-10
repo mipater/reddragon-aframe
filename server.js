1554eed36ff26e85c47516c32ff44944c18bc1a4
Michele Paternoster <pianomaik@gmail.com>
2021-04-10 11:02:03 +0200

    heroku

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
-app.use(express.static('./dist/reddragon-vrmuseum'));
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', {root: 'dist/reddragon-vrmuseum/'}
-  );
+
+app.use(express.static(__dirname + '/dist/reddragon-vrmuseum'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/reddragon-vrmuseum/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);

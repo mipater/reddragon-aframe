1834a4b25ad93bf2e6532cfb4747a16b63ce453e
Michele Paternoster <pianomaik@gmail.com>
2021-04-14 22:29:37 +0200

    homepage backoffice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppComponent } from './app.component';
 import { AframeComponent } from './aframe/aframe.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent, pathMatch: 'full'},
   {path: 'aframe', component: AframeComponent},
+  {path: '**', redirectTo: ''}
  ];
 
 @NgModule({
   declarations: [
     AppComponent,
     AframeComponent,
-    HomepageComponent
+    HomepageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
